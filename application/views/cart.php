e26e556fcc61d7438647768273d8c1ed271863ef
Budiman Putra Jaya <42717955+lieahau@users.noreply.github.com>
2019-05-18 23:08:01 +0700

    Added dynamic list cart

--- application/views/cart.php
+++ application/views/cart.php
@@ -8,10 +8,9 @@
     </head>
     <body>
         <?php echo $initial['navbar']; ?>
-<?php var_dump('listcart'); ?>
         <div class="container">
             <div class="row">
-                <div class="col-10 mx-auto py-5">
+                <div class="col-12 mx-auto py-5">
                     <section class="shopping-cart dark">
                         <div class="container">
                             <div class="block-heading">
@@ -22,80 +21,58 @@
                                 <div class="row">
                                     <div class="col-md-12 col-lg-8">
                                         <div class="items">
-                                            <div class="product">
-                                                <div class="row">
-                                                    <div class="col-md-2">
-                                                        <img class="img-fluid mx-auto d-block image" src='<?php echo base_url('assets/database/shoes/Nike/5cdc552f52cf5.jpg'); ?>'>
-                                                    </div>
-                                                    <div class="col-md-10">
-                                                        <div class="info">
-                                                            <div class="row">
-                                                                <div class="col-md-4 product-name">
-                                                                    <div class="product-name">
-                                                                        <a href="#">Nama Sepatu</a>
-                                                                        <div class="product-info">
-                                                                            <div><span class="value">Running Shoes</span></div>
-                                                                            <div>Size: <span class="value">40</span></div>
+                                        <?php
+                                            if(isset($listcart)){
+                                                foreach($listcart as $product)
+                                                {
+                                        ?>
+                                                    <div class="product">
+                                                        <div class="row">
+                                                            <div class="col-md-2">
+                                                                <img class="img-fluid mx-auto d-block image" src='<?php echo base_url("assets/database/shoes/".$product->detail->merk."/".$product->detail->image); ?>'>
+                                                            </div>
+                                                            <div class="col-md-10">
+                                                                <div class="info">
+                                                                    <div class="row">
+                                                                        <div class="col-md-4 product-name">
+                                                                            <div class="product-name">
+                                                                                <h5><?php echo $product->detail->Nama; ?></h5>
+                                                                                <div class="product-info">
+                                                                                    <div><span class="value"><?php echo $product->detail->jenis; ?></span></div>
+                                                                                    <div>Size: <span class="value"><?php echo $product->detail->ukuran; ?></span></div>
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+                                                                        <div class="col-md-2 quantity">
+                                                                            <p>Quantity:</p>
+                                                                            <p><?php echo $product->jumlah; ?></p>
+                                                                        </div>
+                                                                        <div class="col-md-5 price">
+                                                                            <span><?php echo number_format($product->harga,2,',','.'); ?></span>
+                                                                        </div>
+                                                                        <div class="col-md-1 delete">
+                                                                            <button id="delete" type="button" class="btn"><i class="fas fa-trash"></i></button>
                                                                         </div>
                                                                     </div>
-                                                                </div>
-                                                                <div class="col-md-2 quantity">
-                                                                    <p>Quantity:</p>
-                                                                    <p>1</p>
-                                                                </div>
-                                                                <div class="col-md-5 price">
-                                                                    <span>Rp. 1.000.000</span>
-                                                                </div>
-                                                                <div class="col-md-1 delete">
-                                                                    <button id="delete" type="button" class="btn"><i class="fas fa-trash"></i></button>
                                                                 </div>
                                                             </div>
                                                         </div>
                                                     </div>
-                                                </div>
-                                            </div>
-                                            <div class="product">
-                                                <div class="row">
-                                                    <div class="col-md-2">
-                                                        <img class="img-fluid mx-auto d-block image" src='<?php echo base_url('assets/database/shoes/Nike/5cdc552f52cf5.jpg'); ?>'>
-                                                    </div>
-                                                    <div class="col-md-10">
-                                                        <div class="info">
-                                                            <div class="row">
-                                                                <div class="col-md-4 product-name">
-                                                                    <div class="product-name">
-                                                                        <a href="#">Nama Sepatu</a>
-                                                                        <div class="product-info">
-                                                                            <div><span class="value">Running Shoes</span></div>
-                                                                            <div>Size: <span class="value">40</span></div>
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                                <div class="col-md-2 quantity">
-                                                                    <p>Quantity:</p>
-                                                                    <p>2</p>
-                                                                </div>
-                                                                <div class="col-md-5 price">
-                                                                    <span>Rp. 2.000.000</span>
-                                                                </div>
-                                                                <div class="col-md-1 delete">
-                                                                    <button type="button" class="btn"><i class="fas fa-trash"></i></button>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
+                                                    <hr/>
+                                                <?php
+                                                }
+                                            }
+                                        ?>
                                         </div>
                                     </div>
                                     <div class="col-md-12 col-lg-4">
                                         <div class="summary">
                                             <h3>Summary</h3>
-                                            <div class="summary-item"><span class="text">Subtotal</span><span class="price">Rp. 3.000.000</span></div>
-                                            <div class="summary-item"><span class="text">Discount</span><span class="price">Rp. 0</span></div>
-                                            <div class="summary-item"><span class="text">Shipping</span><span class="price">Rp. 0</span></div>
-                                            <div class="summary-item"><span class="text">Total</span><span class="price">Rp. 3.000.000</span></div>
-                                            <button type="button" id="checkout" class="btn btn-primary btn-lg btn-block">Checkout</button>
+                                            <div class="summary-item"><span class="text">Subtotal</span><span class="price"><?php echo "Rp. ".number_format($totalprice,2,',','.'); ?></span></div>
+                                            <div class="summary-item"><span class="text">Discount</span><span class="price">Rp. 0,00</span></div>
+                                            <div class="summary-item"><span class="text">Shipping</span><span class="price">Rp. 0,00</span></div>
+                                            <div class="summary-item"><span class="text">Total</span><span class="price"><?php echo "Rp. ".number_format($totalprice,2,',','.'); ?></span></div>
+                                            <button type="button" id="checkout" class="btn btn-primary btn-lg btn-block" <?php if(!isset($listcart)) echo 'disabled'; ?> >Checkout</button>
                                         </div>
                                     </div>
                                 </div> 
